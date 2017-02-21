e6dae5d0859758a0c7917c1203c96b0e3708f4a4
sj8716643 <sj8716643@126.com>
2017-02-21 15:31:10 +0000

    add files

---
+++ src/main/java/org/cloudfoundry/samples/music/tools/ConvertingTools.java
@@ -0,0 +1,79 @@
+package org.cloudfoundry.samples.music.tools;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.cloudfoundry.samples.music.domain.Album;
+import org.cloudfoundry.samples.music.domain.RandomIdGenerator;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * Created by shijian on 07/02/2017.
+ */
+public class ConvertingTools {
+
+    /**
+     * [Object1, Object2, Object3] => ["stringInObj1", "stringInObj2","stringInObj3"]
+     * @param objects
+     * @return
+     */
+    public static String[] ObjectArraryToStringArray(Object[] objects){
+        int size = objects.length;
+        String [] strings = new String[size];
+
+        for (int i = 0; i < size; i++)
+            strings[i] = objects[i].toString();
+        return strings;
+    }
+
+    /**
+     * [a, b, c] => "a, b, c"
+     * @param strings
+     * @return
+     */
+    public static String StringArrayToString(String[] strings) {
+        StringBuffer sb = new StringBuffer();
+        for(String string : strings) {
+            sb.append("," + string);
+        }
+        sb.deleteCharAt(0);
+        return sb.toString();
+    }
+
+    /**
+     * parsing json string to hashMap
+     * @param jsonString
+     * @return
+     */
+    public static HashMap<String, String> JSONStringToHashMap(String jsonString) {
+
+        HashMap<String, String> res = new HashMap<String, String>();
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = new HashMap<>();
+        try {
+            map = mapper.readValue(jsonString, Map.class);
+        } catch (Exception e) { e.printStackTrace(); }
+        for(Map.Entry<String, Object> entry : map.entrySet()) {
+            res.put(entry.getKey(), entry.getValue().toString());
+        }
+        return res;
+    }
+
+    public static Album HashMapToAlbum(HashMap<String, String> map) {
+
+        Album album = new Album();
+        RandomIdGenerator gen = new RandomIdGenerator();
+        album.setAlbumId(map.get("albumId").trim());
+        album.setArtist(map.get("artist").trim());
+        album.setGenre(map.get("genre").trim());
+        album.setTitle(map.get("title").trim());
+        album.setReleaseYear(map.get("releaseYear").trim());
+        if(map.get("id") == null) {
+            album.setId(gen.generateId());
+        } else {
+            album.setId(map.get("id").trim());
+        }
+        return album;
+
+    }
+
+}
